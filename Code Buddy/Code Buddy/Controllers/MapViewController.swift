38797c6524a3140bdaf3d95b52ef5fa546d53647
Furkan Vural <furkanvural52@gmail.com>
2023-11-10 23:42:27 +0300

    Add mapviewcontroller and create controllers folder

---
+++ Code Buddy/Code Buddy/Controllers/MapViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MapViewController.swift
+//  Code Buddy
+//
+//  Created by furkan vural on 10.11.2023.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
