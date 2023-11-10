eff7350c11a4c1e2f3f983d92ce539a43f47f810
Furkan Vural <furkanvural52@gmail.com>
2023-11-10 23:49:08 +0300

    Add mapview to storyboard and connect to viewcontroller

--- Code Buddy/Code Buddy/Controllers/MapViewController.swift
+++ Code Buddy/Code Buddy/Controllers/MapViewController.swift
@@ -6,9 +6,13 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
