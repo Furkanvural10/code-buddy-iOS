c81f4ed4e0bd85a16fffea7b5b15f63b17baf7cb
Furkan Vural <furkanvural52@gmail.com>
2023-11-11 17:53:14 +0300

    Complete panel view

--- Code Buddy/Code Buddy/Controllers/MapViewController.swift
+++ Code Buddy/Code Buddy/Controllers/MapViewController.swift
@@ -12,22 +12,52 @@ import CoreLocation
 class MapViewController: UIViewController {
     
     @IBOutlet weak var controlPanelView: UIView!
-    
-    
     @IBOutlet weak var mapView: MKMapView!
+    
+    @IBOutlet weak var addImage: UIImageView!
+    @IBOutlet weak var favImage: UIImageView!
+    @IBOutlet weak var locationImage: UIImageView!
+    
     var locationManager = CLLocationManager()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupMap()
         setupControlPanel()
+        setupUI()
         
-
+        
+    }
+    
+    private func setupUI() {
+        
+        // MARK: - Add image clicked
+        let gestureRecognizerForAddImage = UITapGestureRecognizer(target: self, action: #selector(openAddSheet(_:)))
+        self.addImage.addGestureRecognizer(gestureRecognizerForAddImage)
+        self.addImage.isUserInteractionEnabled = true
+        
+        // MARK: - Fav image clicked
+        let gestureRecognizerForFavImage = UITapGestureRecognizer(target: self, action: #selector(openAddSheet(_:)))
+        
+        self.favImage.addGestureRecognizer(gestureRecognizerForFavImage)
+        self.favImage.isUserInteractionEnabled = true
+        
+        // MARK: - location image clicked
+        let gestureRecognizerForLocationImage = UITapGestureRecognizer(target: self, action: #selector(openAddSheet(_:)))
+        self.locationImage.addGestureRecognizer(gestureRecognizerForLocationImage)
+        self.locationImage.isUserInteractionEnabled = true
+        
     }
     
     private func setupControlPanel() {
         self.controlPanelView.layer.cornerRadius = 15
-        self.controlPanelView.backgroundColor = .white.withAlphaComponent(0.2)
+        self.controlPanelView.backgroundColor = .black.withAlphaComponent(0.5)
+        
+        // Image Setup
+        self.addImage.tintColor = .white
+        self.favImage.tintColor = .red
+        self.locationImage.tintColor = .systemBlue
     }
     
     
@@ -39,6 +69,24 @@ class MapViewController: UIViewController {
         mapView.overrideUserInterfaceStyle = .dark
         mapView.showsUserLocation = true
         
+    }
+    
+    
+    @objc private func openAddSheet(_ sender: UITapGestureRecognizer) {
+        guard let tappedImageView = sender.view as? UIImageView else { return }
+        
+        
+        if tappedImageView == addImage {
+            
+            print("Tıklandı: add")
+            
+        } else if tappedImageView == favImage {
+            
+            print("Tıklandı: fav")
+            
+        } else {
+            print("Tıklandı location")
+        }
     }
     
 }
