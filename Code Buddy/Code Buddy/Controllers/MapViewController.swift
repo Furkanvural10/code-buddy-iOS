efb94a87b9db16637ebfa79a65aa6a0254daf0f3
Furkan Vural <furkanvural52@gmail.com>
2023-11-11 16:22:43 +0300

    Add function which get user location and request

--- Code Buddy/Code Buddy/Controllers/MapViewController.swift
+++ Code Buddy/Code Buddy/Controllers/MapViewController.swift
@@ -7,17 +7,43 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapViewController: UIViewController {
     
     
     @IBOutlet weak var mapView: MKMapView!
+    var locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+        
 
     }
     
+    
+    private func setupUI() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        mapView.overrideUserInterfaceStyle = .dark
+        mapView.showsUserLocation = true
+        
+    }
+    
+}
 
-
+extension MapViewController: MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("Location didudate worked")
+        let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.3, longitudeDelta: 0.3)
+        let region = MKCoordinateRegion(center: location, span: span)
+        
+        self.mapView.setRegion(region, animated: true)
+    }
+    
 }
