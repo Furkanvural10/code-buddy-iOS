ae06380c2e058815a7e3744c5e9bd24ed874cad0
Furkan Vural <furkanvural52@gmail.com>
2023-11-16 18:32:51 +0300

    Add places tableview and set title to segmented control

--- Code Buddy/Code Buddy/Scene/Map/PlaceSheet/PlacesViewController.swift
+++ Code Buddy/Code Buddy/Scene/Map/PlaceSheet/PlacesViewController.swift
@@ -9,21 +9,45 @@ import UIKit
 
 class PlacesViewController: UIViewController {
 
+    @IBOutlet weak var placesSegmentedController: UISegmentedControl!
+    
+    @IBOutlet weak var placesTableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        placesTableView.delegate = self
+        placesTableView.dataSource = self
+        setupSegmentedController()
+      
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupSegmentedController() {
+        placesSegmentedController.setTitle("Favorite", forSegmentAt: 0)
+        placesSegmentedController.setTitle("Best", forSegmentAt: 1)
     }
-    */
+ 
+    @IBAction func placesSegmentedControllerClicked(_ sender: Any) {
+    }
+    
+}
 
+extension PlacesViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "XYZ Places"
+        cell.detailTextLabel?.text = "Kadıköy"
+        return cell
+    }
+    
+    
 }
