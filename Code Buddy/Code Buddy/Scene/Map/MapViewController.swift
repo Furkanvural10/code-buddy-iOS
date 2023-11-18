e50a102569a7ca9c1e5fdf938682f0aa058e76ce
Furkan Vural <furkanvural52@gmail.com>
2023-11-19 01:50:23 +0300

    Add custom marker view

--- Code Buddy/Code Buddy/Scene/Map/MapViewController.swift
+++ Code Buddy/Code Buddy/Scene/Map/MapViewController.swift
@@ -20,10 +20,14 @@ final class MapViewController: UIViewController {
     @IBOutlet private weak var personsImage: UIImageView!
     
     private lazy var locationManager = CLLocationManager()
+    private var location: CLLocationCoordinate2D!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        mapView.delegate = self
+        mapView.register(MapPinView.self, forAnnotationViewWithReuseIdentifier: "pin")
+        
         setupMap()
         setupControlPanel()
         setupUI()
@@ -78,6 +82,7 @@ final class MapViewController: UIViewController {
         mapView.overrideUserInterfaceStyle = .dark
         mapView.showsUserLocation = true
         
+        
     }
     
     
@@ -101,34 +106,83 @@ final class MapViewController: UIViewController {
         }
     }
     
+    
+    
 }
 
 extension MapViewController: MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        print("Worked")
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: "pin", for: annotation)
+        pinView = MapPinView(annotation: annotation, reuseIdentifier: "pin")
+        return pinView
+    }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         
         let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
         let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
         let region = MKCoordinateRegion(center: location, span: span)
-        
+        self.location = location
         self.mapView.setRegion(region, animated: true)
         locationManager.stopUpdatingLocation()
     }
+}
+
+
+class MapPinView: MKAnnotationView {
+    private lazy var containerView: UIView = {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 30))
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 16.0
+        return view
+    }()
     
+    private lazy var imageView: UIImageView = {
+        let imageview = UIImageView()
+        imageview.translatesAutoresizingMaskIntoConstraints = false
+        imageview.image = UIImage(systemName: "pencil.circle.fill")
+        imageview.layer.cornerRadius = 8.0
+        imageview.clipsToBounds = true
+        return imageview
+    }()
     
-    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "AnnotationView")
-        
-        if annotationView == nil {
-            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "AnnotationView")
-        }
-        
-        if let title = annotation.title, title == "Deneme" {
-            annotationView?.image = UIImage(systemName: "car")
-        }
-        
-        
-        return annotationView
+    private lazy var bottomCornerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 4.0
+        return view
+    }()
+    
+    // MARK: Initialization
+    override init(annotation: MKAnnotation?, reuseIdentifier: String?) {
+        print("Inıt marker")
+        super.init(annotation: annotation, reuseIdentifier: reuseIdentifier)
+        setupView()
     }
     
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+        containerView.addSubview(bottomCornerView)
+        bottomCornerView.topAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -15.0).isActive = true
+        bottomCornerView.centerXAnchor.constraint(equalTo: containerView.centerXAnchor).isActive = true
+        bottomCornerView.widthAnchor.constraint(equalToConstant: 24).isActive = true
+        bottomCornerView.heightAnchor.constraint(equalToConstant: 24).isActive = true
+        
+        let angle = (39.0 * CGFloat.pi) / 180
+        let transform = CGAffineTransform(rotationAngle: angle)
+        bottomCornerView.transform = transform
+        
+        addSubview(containerView)
+        containerView.addSubview(imageView)
+        imageView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 8.0).isActive = true
+        imageView.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 8.0).isActive = true
+        imageView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -8.0).isActive = true
+        imageView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -8.0).isActive = true
+    }
 }
