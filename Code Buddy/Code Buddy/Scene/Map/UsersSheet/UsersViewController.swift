1da06c699d590944a5be60d2ad157e26245049a4
Furkan Vural <furkanvural52@gmail.com>
2024-02-05 13:52:29 +0300

    Change collectionview background color from white to black

--- Code Buddy/Code Buddy/Scene/Map/UsersSheet/UsersViewController.swift
+++ Code Buddy/Code Buddy/Scene/Map/UsersSheet/UsersViewController.swift
@@ -4,20 +4,30 @@ import UIKit
 
 final class UsersViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var searchBar: UISearchBar!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .black
         setupUI()
     }
     
     private func setupUI() {
+        
+        // MARK: - SearchBar Configuration
+        searchBar.searchTextField.keyboardAppearance = .dark
+        searchBar.barStyle = .black
+        
+        // MARK: - CollectionView Configuration
         collectionView.delegate = self
         collectionView.dataSource = self
+        collectionView.backgroundColor = .black
         
+        
+        
+
         let collectionViewLayout = UICollectionViewFlowLayout()
         collectionViewLayout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
         collectionViewLayout.minimumInteritemSpacing = 0
@@ -41,29 +51,12 @@ extension UsersViewController: UICollectionViewDelegate, UICollectionViewDataSou
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "userCollectionViewID", for: indexPath) as! UserCollectionViewCell
-        
-        cell.userNameLabel.text = "Furkan Vural"
-        cell.userTitleLabel.text = "Frontend Developers"
-        
-        let originalImage = UIImage(named: "fv")
-        let resizedImage = originalImage?.resized(to: CGSize(width: 30, height: 30))
-        cell.userImageView.image = resizedImage
-        cell.userImageView.layer.cornerRadius = cell.userImageView.frame.height / 2
-        cell.userImageView.clipsToBounds = true
-            
-        
-        cell.layer.borderColor = UIColor.clear.cgColor
-        cell.layer.borderWidth = 0.4
-        cell.layer.cornerRadius = 10
-        
+
+        let image = UIImage(named: "fv")
+        cell.setCell(image: image!, name: "Furkan Vural", title: "iOS Developer")
         return cell
     }
     
-}
-extension UIImage {
-    func resized(to size: CGSize) -> UIImage {
-        return UIGraphicsImageRenderer(size: size).image { _ in
-            draw(in: CGRect(origin: .zero, size: size))
-        }
-    }
+    
+    
 }
