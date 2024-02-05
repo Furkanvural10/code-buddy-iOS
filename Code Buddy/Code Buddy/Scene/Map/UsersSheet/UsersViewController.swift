bf1209d06f8698dce01c589aa33cf212788cf109
Furkan Vural <furkanvural52@gmail.com>
2024-02-05 14:28:57 +0300

    Add mock data for collectionView

--- Code Buddy/Code Buddy/Scene/Map/UsersSheet/UsersViewController.swift
+++ Code Buddy/Code Buddy/Scene/Map/UsersSheet/UsersViewController.swift
@@ -3,15 +3,49 @@
 import UIKit
 
 final class UsersViewController: UIViewController {
+    
+    enum Section {
+        case main
+    }
 
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var searchBar: UISearchBar!
+    var dataSource: UICollectionViewDiffableDataSource<Section, String>!
+    
+    
+    // MOCK DATA
+    let mockNames: [String] = [
+        "Ahmet",
+        "Mehmet",
+        "Ayşe",
+        "Fatma",
+        "Mustafa",
+        "Zeynep",
+        "Ali",
+        "Şehrazat",
+        "Cem",
+        "Elif"
+    ]
+    
+    let mockSoftwareTitles: [String] = [
+        "CodeMaster",
+        "DataForge",
+        "ByteGenius",
+        "LogicCraft",
+        "SwiftNinja",
+        "AppWizard",
+        "TechSculptor",
+        "InnovateHub",
+        "BitHarbor",
+        "EpicCoder"
+    ]
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .black
         setupUI()
+        
     }
     
     private func setupUI() {
@@ -24,9 +58,6 @@ final class UsersViewController: UIViewController {
         collectionView.delegate = self
         collectionView.dataSource = self
         collectionView.backgroundColor = .black
-        
-        
-        
 
         let collectionViewLayout = UICollectionViewFlowLayout()
         collectionViewLayout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
@@ -46,17 +77,17 @@ final class UsersViewController: UIViewController {
 extension UsersViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 20
+        return mockNames.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "userCollectionViewID", for: indexPath) as! UserCollectionViewCell
 
         let image = UIImage(named: "fv")
-        cell.setCell(image: image!, name: "Furkan Vural", title: "iOS Developer")
+        let name = mockNames[indexPath.row]
+        let title = mockSoftwareTitles[indexPath.row]
+        cell.setCell(image: image!, name: name, title: title)
         return cell
     }
-    
-    
-    
+
 }
