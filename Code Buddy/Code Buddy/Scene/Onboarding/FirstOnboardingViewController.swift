36178f2b11e2f3f031bebcf180034748f68e8bdf
Furkan Vural <furkanvural52@gmail.com>
2024-02-28 15:28:59 +0300

    Connect first onboarding page uı elements

--- Code Buddy/Code Buddy/Scene/Onboarding/FirstOnboardingViewController.swift
+++ Code Buddy/Code Buddy/Scene/Onboarding/FirstOnboardingViewController.swift
@@ -5,13 +5,17 @@
 //  Created by furkan vural on 28.02.2024.
 //
 
+#warning("Fix hard coded")
+
 import UIKit
 
 final class FirstOnboardingViewController: UIViewController {
     
     @IBOutlet weak var backgroundImage: UIImageView!
     
-
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
@@ -20,12 +24,31 @@ final class FirstOnboardingViewController: UIViewController {
     }
     
     private func setupUI() {
+        
+        // MARK: - Background Image Configuration
         backgroundImage.image = UIImage(named: "img_onboarding1")
         backgroundImage.layer.zPosition = -1
         backgroundImage.contentMode = .scaleAspectFit
+        
+        // MARK: - Title Label Configuration
+        titleLabel.text = "DISCOVER NEARBY DEVELOPERS."
+        titleLabel.adjustsFontSizeToFitWidth = true
+        
+        // MARK: - Message Label Configuration
+        messageLabel.text = "Explore the map to see other developers around you."
+        messageLabel.adjustsFontSizeToFitWidth = true
+        
+        
+        
+        
     }
     
-
+    
+    
+    @IBAction func continueButtonClicked(_ sender: Any) {
+        
+    }
+    
 
 
 }
